bd8988ae5fc1f354cb71199c8ad4ec6e10b4ba8b
Juan-vargas98 <jortizvargas1603@gmail.com>
2025-10-03 16:43:08 -0500

    Fix final V2: Inyeccion directa del controlador en rutas para evitar fallo de require

--- routes/mediaRoutes.js
+++ routes/mediaRoutes.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const mediaController = require('../controllers/mediaController');
+// NOTA: No hacemos 'const mediaController = require(...)' para evitar fallos de path.
 
-router.get('/', mediaController.getMedia);
-router.get('/:id', mediaController.getMediaById);
-router.post('/', mediaController.createMedia);
-router.put('/:id', mediaController.updateMedia);
-router.delete('/:id', mediaController.deleteMedia);
+router.get('/', require('../controllers/mediaController').getMedia);
+router.get('/:id', require('../controllers/mediaController').getMediaById);
+router.post('/', require('../controllers/mediaController').createMedia);
+router.put('/:id', require('../controllers/mediaController').updateMedia);
+router.delete('/:id', require('../controllers/mediaController').deleteMedia);
 
 module.exports = router;
