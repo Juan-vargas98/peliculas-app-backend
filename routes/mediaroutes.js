0db421f3f7c1871129466eae172ab84083c95cc4
Juan-vargas98 <jortizvargas1603@gmail.com>
2025-10-03 22:08:26 -0500

    Fix final: Corregida la capitalizacion del require de mediaController a minuscula en mediaroutes.js

--- routes/mediaroutes.js
+++ routes/mediaroutes.js
@@ -2,10 +2,10 @@ const express = require('express');
 const router = express.Router();
 // NOTA: No hacemos 'const mediaController = require(...)' para evitar fallos de path.
 
-router.get('/', require('../controllers/mediaController').getMedia);
-router.get('/:id', require('../controllers/mediaController').getMediaById);
-router.post('/', require('../controllers/mediaController').createMedia);
-router.put('/:id', require('../controllers/mediaController').updateMedia);
-router.delete('/:id', require('../controllers/mediaController').deleteMedia);
+router.get('/', require('../controllers/mediacontroller').getMedia);
+router.get('/:id', require('../controllers/mediacontroller').getMediaById);
+router.post('/', require('../controllers/mediacontroller').createMedia);
+router.put('/:id', require('../controllers/mediacontroller').updateMedia);
+router.delete('/:id', require('../controllers/mediacontroller').deleteMedia);
 
 module.exports = router;
